5d904c9f1171f4dcd0b2466367792bb327ff808a
Tironi_Lab_Thais <rafaelash3211@gmail.com>
2026-07-08 10:06:12 -0300

    add: 08 pronto

--- ex_08.php
+++ ex_08.php
@@ -7,9 +7,15 @@ $lista = explode(",", $nomes);
 sort($lista);
 
 return $lista;
+
 }
 
 $nomes = "Juana, Rafael, Luiz, Nicole";
 
-echo "Nomes: $nomes <br>";
-echo "Nomes em ordem alfabética: $lista <br>";
+$resultado = ordenarNomes($nomes);
+
+echo "Nomes: $nomes <br> <br>";
+
+foreach ($resultado as $nome) {
+    echo $nome . "<br>";
+}
