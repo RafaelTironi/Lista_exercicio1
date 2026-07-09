972b971db0dc37209e5e0216eec13b6eceee347d
Rafael_2213 <rafael_tironi1@estudante.sesisenai.org.br>
2026-07-09 08:06:24 -0300

    up: arrumei imc

--- funcoes.php/calcular_imc.php
+++ funcoes.php/calcular_imc.php
@@ -7,5 +7,6 @@ function calcularIMC($peso, $altura){
 $peso = 62;
 $altura = 1.72;
 
-echo "Peso $peso e altura $altura .";
-echo "IMC = " .calcularIMC();
+echo "Peso: $peso kg<br>";
+echo "Altura: $altura m<br>";
+echo "IMC = " . calcularIMC($peso, $altura);
